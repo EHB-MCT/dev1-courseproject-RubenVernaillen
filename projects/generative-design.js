6e161d083549c5f8b393732d2e439baf65396ffb
Ruben Vernaillen <Ruben.vernaillen@student.ehb.be>
2024-12-17 11:23:32 +0100

    eye simple design

--- projects/generative-design.js
+++ projects/generative-design.js
@@ -2,42 +2,14 @@
 "use strict";
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
+let width = context.canvas.width;
+let height = context.canvas.height;
+let x = width / 2
+let y = height / 2
+draweye();
 
-
-draw();
-function draw() {
-
-    context.lineWidth = 5;
-    context.fillstyle = 'white'
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-
-
-
-    for (let j = 0; j < 10; j++) {
-        for (let i = 0; i < 10; i++) {
-
-
-            if (Utils.randomNumber(0, 1)) {
-
-                strokeCircle(canvas.width / 2, canvas.height / 2, i * 10);
-            } else {
-
-                strokeCircle(canvas.width / 2, canvas.height / 2, i * 50);
-            }
-        }
-    }
-}
-
-
-function strokeCircle(x, y, radius) {
-
-    let red = Math.random() * 255;
-    let green = Math.random() * 255;
-    let blue = Math.random() * 255;
-
-    context.strokeStyle = "rgb(" + red + "," + green + "," + blue + ")";
-
-
-    Utils.strokeCircle(x, y, radius);
+function draweye() {
+    Utils.strokeEllipse(x, y, 100, 50)
+    Utils.strokeEllipse(x, y, 10, 10)
+    Utils.strokeEllipse(x, y, 50, 50)
 }
