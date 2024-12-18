a4bfd856b06e7a53c2fa27438272a056c50eb9ab
Ruben Vernaillen <Ruben.vernaillen@student.ehb.be>
2024-12-18 11:29:40 +0100

    function create eye

--- projects/generative-design.js
+++ projects/generative-design.js
@@ -4,57 +4,70 @@ import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
-let eyelist = []
+let eyelist = [];
+for (let i = 0; i < 10; i++) {
+    let x = Utils.randomNumber(0, width)
+    let y = Utils.randomNumber(0, height)
+    eyelist.draweye.push(createye(x, y))
+}
+
+function createye(x, y) {
+
+    return {
+        oogX: x,
+        oogY: y,
+        irisX: x,
+        irisY: y,
+        iriskleur: Utils.rgb(Utils.randomNumber(0, 360), Utils.randomNumber(0, 360), Utils.randomNumber(0, 360)),
+
+    }
+}
+
+
+
+
 
 draweye();
+
 
 
 function draweye() {
 
 
-    let x = Utils.randomNumber(0, width)
-    let y = Utils.randomNumber(0, height)
+
 
     context.lineWidth = 1;
-    Utils.strokeEllipse(x, y, 50, 25)
+    Utils.strokeEllipse(eyelist.oogX, eyelist.oogY, 50, 25)
     context.fillStyle = "rgba(0, 0, 0, 0.1)"
-    Utils.fillEllipse(x, y, 45, 20)
-    context.fillStyle = Utils.rgb(Utils.randomNumber(0, 360), Utils.randomNumber(0, 360), Utils.randomNumber(0, 360))
-    Utils.fillEllipse(x, y, 25, 25)
+    Utils.fillEllipse(eyelist.oogX, eyelist.oogX, 45, 20)
+    context.fillStyle = eyelist.iriskleur
+    Utils.fillEllipse(eyelist.irisX, eyelist.irisY, 25, 25)
     context.fillStyle = "#000000"
-    Utils.fillEllipse(x, y, 8, 8)
+    Utils.fillEllipse(eyelist.irisX, eyelist.irisY, 8, 8)
     context.lineWidth = 0.5;
-    Utils.drawLine(x, y, x + 15, y + 15)
-    Utils.drawLine(x, y, x - 15, y - 15)
-    Utils.drawLine(x, y, x + 15, y - 15)
-    Utils.drawLine(x, y, x - 15, y + 15)
-    Utils.drawLine(x, y, x, y - 20)
-    Utils.drawLine(x, y, x - 20, y)
-    Utils.drawLine(x, y, x, y + 20)
-    Utils.drawLine(x, y, x + 20, y)
-    Utils.drawLine(x, y, x + 14, y - 6)
-    Utils.drawLine(x, y, x - 14, y - 6)
-    Utils.drawLine(x, y, x + 14, y + 6)
-    Utils.drawLine(x, y, x - 14, y - 6)
-    Utils.drawLine(x, y, x - 14, y + 6)
-    Utils.drawLine(x, y, x + 6, y - 14)
-    Utils.drawLine(x, y, x - 6, y - 14)
-    Utils.drawLine(x, y, x + 6, y + 14)
-    Utils.drawLine(x, y, x - 6, y - 14)
-    Utils.drawLine(x, y, x - 6, y + 14)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 15, eyelist.irisY + 15)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 15, eyelist.irisY - 15)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 15, eyelist.irisY - 15)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 15, eyelist.irisY + 15)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX, eyelist.irisY - 20)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 20, eyelist.irisY)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX, eyelist.irisY + 20)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 20, eyelist.irisY)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 14, eyelist.irisY - 6)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 14, eyelist.irisY - 6)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 14, eyelist.irisY + 6)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 14, eyelist.irisY - 6)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 14, eyelist.irisY + 6)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 6, eyelist.irisY - 14)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 6, eyelist.irisY - 14)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX + 6, eyelist.irisY + 14)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 6, eyelist.irisY - 14)
+    Utils.drawLine(eyelist.irisX, eyelist.irisY, eyelist.irisX - 6, eyelist.irisY + 14)
 
     context.fillStyle = "#FFFFFF"
 
-    Utils.fillEllipse(x - 14, y - 14, 8, 8)
-    Utils.fillEllipse(x + 14, y + 14, 4, 4)
-
-}
-
-for (let i = 0; i < 10; i++) {
-    let x = Utils.randomNumber(0, width)
-    let y = Utils.randomNumber(0, height)
-    eyelist.push(draweye(x, y))
-
+    Utils.fillEllipse(eyelist.irisX - 14, eyelist.irisY - 14, 8, 8)
+    Utils.fillEllipse(eyelist.irisX + 14, eyelist.irisY + 14, 4, 4)
 
 }
 
